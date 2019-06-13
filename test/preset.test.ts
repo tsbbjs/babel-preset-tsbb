b05b1c276fe333c727bd2f9fccbd1b2d1c64162a
jaywcjlove <398188662@qq.com>
2019-06-13 10:46:19 +0800

    Add test case.

---
+++ test/preset.test.ts
@@ -0,0 +1,7 @@
+import preset from '../';
+
+it('preset test case', () => {
+  const conf = preset({});
+  expect(Object.keys(conf)).toEqual(['presets', 'plugins']);
+  expect(conf.presets.length).toBe(2);
+});

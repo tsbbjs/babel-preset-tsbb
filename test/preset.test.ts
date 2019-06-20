80c5a154516d19276c51c7160b0c0266724b913c
jaywcjlove <398188662@qq.com>
2019-06-20 13:47:02 +0800

    Modify test case.

--- test/preset.test.ts
+++ test/preset.test.ts
@@ -1,7 +1,7 @@
 import preset from '../';
 
 it('preset test case', () => {
-  const conf = preset({});
+  const conf = preset({}, {});
   expect(Object.keys(conf)).toEqual(['presets', 'plugins']);
   expect(conf.presets.length).toBe(2);
 });
